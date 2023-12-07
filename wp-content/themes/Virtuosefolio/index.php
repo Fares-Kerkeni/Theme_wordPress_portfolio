f3fb72a7ee198c2d4f5d16706b051b49c0debf5f
Fares Kerkeni <kerkenif11@gmail.com>
2023-12-07 22:40:58 +0100

    works

--- wp-content/themes/Virtuosefolio/index.php
+++ wp-content/themes/Virtuosefolio/index.php
@@ -1,25 +1,55 @@
-<?php get_header(); ?>
-<?php if(have_posts()): ?>
-<div class="all_posts">
-    <?php while(have_posts()): the_post(); ?>
+<?php
+get_header();
+?>
 
-    <div class="post">
-        <!-- // Pour afficher le titre et le contenu de chaque article, il faut ajouter
-        les fonctions the_title() et the_content() dans le fichier index.php -->
-        <!-- // the permission cree un lien pour chaque article -->
-    
-        
-            <?php the_post_thumbnail(); ?>
-            <?php the_title(); ?>
-            <?php the_content(); ?>
-
-       
-
-        <!-- <a href="<?php the_permalink()?>"> suite </a> -->
-    </div>
-
-    <?php endwhile; ?>
+<div class="title_woks">
+    <h1>Works</h1>
 </div>
-<?php endif; ?>
 
-<?php get_footer(); ?>
+<?php
+// Replace 'your_category_slug' with the slug of the category you want to display
+query_posts('category_name=Project');
+
+if (have_posts()): ?>
+
+    <div class="all_posts">
+
+        <?php
+        $post_count = 0; // Counter variable
+
+        while (have_posts() && $post_count < 4): the_post(); ?>
+
+            <div class="post">
+                <!-- Your existing post content -->
+
+                <?php $post_count++; // Increment the counter ?>
+
+                <div class="container_image">
+                    <?php the_post_thumbnail(); ?>
+                </div>
+                <div class="container_title_view">
+                    <div class="container_title">
+                        <?php the_title(); ?>
+                    </div>
+                    <div class="container_tags">
+                        <?php
+                        $tags = get_the_tags();
+                        if ($tags) {
+                            foreach ($tags as $tag) {
+                                echo '<p>'.esc_html($tag->name).'</p>';
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+
+        <?php endwhile; ?>
+    </div>
+<?php endif;
+
+// Reset the query to the main loop
+wp_reset_query();
+
+get_footer();
+?>
